0d1483e8ece6b5b9c3eafc9ed6df951ff37c6fbe
Thomas Gerbet <thomas@gerbet.me>
2025-02-23 18:25:11 +0100

    flake.lock: update

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
   outputs = { self, nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let pkgs = nixpkgs.legacyPackages.${system}; in {
-        devShell = pkgs.mkShell {
+        devShell = pkgs.mkShellNoCC {
           packages = [
             pkgs.go
             pkgs.cosign
